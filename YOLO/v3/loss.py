2c5bb6aeb5ccea1608ea02475cfdb8cc3ce23249
Jalbin1307 <az13077@gmail.com>
2022-03-07 11:51:06 +0900

    docs : Modify comments

--- YOLO/v3/loss.py
+++ YOLO/v3/loss.py
@@ -20,30 +20,27 @@ class YoloLoss(nn.Module):
         self.lambda_box = 10
 
     def forward(self, predictions, target, anchors):
-        # Check where obj and noobj (we ignore if target == -1)
+        # Check where obj and noobj (ignore if target == -1)
         obj = target[..., 0] == 1  # in paper this is Iobj_i
         noobj = target[..., 0] == 0  # in paper this is Inoobj_i
 
-        # ======================= #
+
         #   FOR NO OBJECT LOSS    #
-        # ======================= #
 
         no_object_loss = self.bce(
             (predictions[..., 0:1][noobj]), (target[..., 0:1][noobj]),
         )
 
-        # ==================== #
+        
         #   FOR OBJECT LOSS    #
-        # ==================== #
-
+       
         anchors = anchors.reshape(1, 3, 1, 1, 2)
         box_preds = torch.cat([self.sigmoid(predictions[..., 1:3]), torch.exp(predictions[..., 3:5]) * anchors], dim=-1)
         ious = intersection_over_union(box_preds[obj], target[..., 1:5][obj]).detach()
         object_loss = self.mse(self.sigmoid(predictions[..., 0:1][obj]), ious * target[..., 0:1][obj])
 
-        # ======================== #
+
         #   FOR BOX COORDINATES    #
-        # ======================== #
 
         predictions[..., 1:3] = self.sigmoid(predictions[..., 1:3])  # x,y coordinates
         target[..., 3:5] = torch.log(
@@ -51,9 +48,8 @@ class YoloLoss(nn.Module):
         )  # width, height coordinates
         box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])
 
-        # ================== #
+
         #   FOR CLASS LOSS   #
-        # ================== #
 
         class_loss = self.entropy(
             (predictions[..., 5:][obj]), (target[..., 5][obj].long()),
